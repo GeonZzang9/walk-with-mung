db566574c7d13a36b91d0770e34b159c8a10b32a
GeonZzang9 <slanjtka1@naver.com>
2025-12-10 21:03:16 +0900

    Migrate backend from SQLite to MySQL (RDS, dotenv)

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -14,4 +15,3 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;
-
